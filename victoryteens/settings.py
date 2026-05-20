4e457cd5df84a7f6d421160d0ad7920e842a4555
Alphaalimamy <alphaalimamykamara@gmail.com>
2026-05-21 02:57:57 +0800

    Database configur

--- victoryteens/settings.py
+++ victoryteens/settings.py
@@ -32,7 +32,7 @@ if not SECRET_KEY:
     raise Exception("SECRET_KEY is not set in environment variables")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = [
